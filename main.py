ac05a865b93dbe63a63de9f92c213558ba342c5d
Кирилл <129333736+Kirill25052008@users.noreply.github.com>
2025-09-30 14:32:35 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+number_1 = int(input("Введите первое число "))
+number_2 = int(input("Введите второе число "))
+
+if number_1 > number_2:
+    print("Число ",number_1, ">",number_2)
+elif number_1 < number_2:
+    print("Число ",number_1, "<",number_2)
+else:
+    print("Число ",number_1, "=",number_2)
