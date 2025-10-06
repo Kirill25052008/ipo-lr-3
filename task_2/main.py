17b738c130623eddf274e59bbb8ad03ad46ca8aa
Кирилл <129333736+Kirill25052008@users.noreply.github.com>
2025-10-06 21:40:02 +0300

    Update main.py

---
+++ task_2/main.py
@@ -0,0 +1,4 @@
+number1 = int(input("Введите число1 "))# Просим ввести первое число с клавиатуры
+number2 = int(input("Введите число2 "))# Просим ввести второе число с клавиатуры
+
+print("Число", number1, "= числа", number2) if number1 == number2 else print("Меньшее число - ", min(number1, number2))
